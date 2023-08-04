b6eeaad757ae77c94bb696e896282590f37de936
XFernando43 <102422211+XFernando43@users.noreply.github.com>
2023-08-04 11:54:52 -0500

    add: Application DbContext and category class

---
+++ Kombox.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Kombox.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kombox.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+            
+        }
+
+            public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Action" },
+                new Category { CategoryId = 2, Name = "Action" }
+                );
+        }
+    }
+}
