37c0faba74e8981f2ece79dc693450db8e6bb103
XFernando43 <102422211+XFernando43@users.noreply.github.com>
2023-08-04 17:38:40 -0500

    add: method get controller categories

--- Kombox.API/Controllers/CategoryController.cs
+++ Kombox.API/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Kombox.DataAccess.Repository.Interfaces;
+using Kombox.Models.Models;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,14 +10,39 @@ namespace Kombox.API.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        public CategoryController() { 
-        
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
         }
+
+
+
         // GET: api/<CategoryController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                List<Category> categories = _unitOfWork.categoryRepository.GetAll().ToList();
+                Console.WriteLine(categories.Count);
+
+                return Ok(new
+                {
+                    success = true,
+                    Categories = categories
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new
+                {
+                    success = false,
+                    Message = ex.Message
+                });
+            }
+            
         }
 
         // GET api/<CategoryController>/5
