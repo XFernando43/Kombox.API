f036a29d5df128873c8afc5e3074f8de02a6c5f7
XFernando43 <102422211+XFernando43@users.noreply.github.com>
2023-08-06 19:47:32 -0500

    add: security tokens

---
+++ Kombox.API/Controllers/UserController.cs
@@ -0,0 +1,74 @@
+﻿using Kombox.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Kombox.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IConfiguration _configuration { get; set; }
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public dynamic Login([FromBody] Object optData)
+        {
+            var data = JsonConvert.DeserializeObject<dynamic>(optData.ToString());
+
+            string user = data.usuario.ToString();
+            string password = data.password.ToString();
+
+            Usuario usuario = Usuario.DB().Where(x => x.usuario == user && x.password == password).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return new
+                {
+                    status = false,
+                    message = "Credenciales Incorrectas",
+                    results = ""
+                };
+            }
+
+            var jwt = _configuration.GetSection("JWT").Get<Jwt>();
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, jwt.Subject),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim("IdUser",usuario.IdUser),
+                new Claim("usuario",usuario.usuario),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                jwt.Issuer,
+                jwt.Audience,
+                claims,
+                expires: DateTime.Now.AddMinutes(4),
+                signingCredentials: signIn
+              );
+
+            return new
+            {
+                success = true,
+                message = "Exito",
+                result = new JwtSecurityTokenHandler().WriteToken(token)
+            };
+
+
+        }
+    }
+}
---
+++ Kombox.Models/Models/Usuario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kombox.Models.Models
+{
+    public class Usuario
+    {
+        public string IdUser { get; set; }
+        public string usuario { get; set; }
+        public string password { get; set; }
+        public string Rol { get; set; }
+
+        public static List<Usuario> DB()
+        {
+            var list = new List<Usuario>()
+            {
+                new Usuario()
+                {
+                    IdUser = "1",
+                    usuario = "Chess",
+                    password = "Mierdamierda",
+                    Rol = "Admin",
+                },
+                new Usuario()
+                {
+                    IdUser = "2",
+                    usuario = "Glam",
+                    password = "Mierdamierda",
+                    Rol = "Employee",
+                },
+                new Usuario()
+                {
+                    IdUser = "3",
+                    usuario = "Fercho",
+                    password = "Mierdamierda",
+                    Rol = "Asesor"
+                }
+            };
+
+            return list;
+        }
+    }
+}
