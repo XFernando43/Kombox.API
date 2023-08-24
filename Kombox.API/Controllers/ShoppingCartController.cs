2d5533b6a4b278946bceb99cf4b7ec8aaf9f9b5e
XFernando43 <102422211+XFernando43@users.noreply.github.com>
2023-08-23 23:35:33 -0500

    Update ShoppingCartController.cs

--- Kombox.API/Controllers/ShoppingCartController.cs
+++ Kombox.API/Controllers/ShoppingCartController.cs
@@ -99,7 +99,7 @@ namespace Kombox.API.Controllers
                     ShoppingCartId = item.ShoppingCartId,
                 };
 
-                // pendiente el acutalizar la cantidad de del producto del item cart deasd   dasda  das   dasd  wsda
+                // pendiente el acutalizar la cantidad de del producto del item cart 
 
                 _unitOfWork.itemCartRepository.Add(aux);
                 _unitOfWork.Save();
